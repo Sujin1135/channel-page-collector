5da4212a619ba56625fcfdb15c3f2c7fda93349c
Sujin1135 <mchead1@naver.com>
2025-02-22 01:22:08 +0900

    feat: applied mutex for parallel execution by using goroutine

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,50 @@ package main
 
 import (
 	"channel-page-collector/internal/collector"
+	"fmt"
+	"log"
+	"sync"
+	"time"
 )
 
 func main() {
 	websiteCollector := collector.NewWebsiteCollector()
-	keyword := "운동"
-	websiteCollector.CollectWithScrolling(keyword, 5)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	start := time.Now()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("start to collect method 1")
+		subscriberNames, err := websiteCollector.CollectWithScrolling("운동", 5)
+		if err != nil {
+			log.Fatal("failed to collect subscriber names", err)
+		}
+
+		fmt.Println("ended to collect method 1")
+		fmt.Println("subscriber names as follow:")
+		for _, name := range subscriberNames {
+			fmt.Println(name)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		fmt.Println("start to collect method 2")
+		subscriberNames, err := websiteCollector.CollectWithScrolling("뷰티", 5)
+		if err != nil {
+			log.Fatal("failed to collect subscriber names", err)
+		}
+
+		fmt.Println("ended to collect method 2")
+		fmt.Println("subscriber names as follow:")
+		for _, name := range subscriberNames {
+			fmt.Println(name)
+		}
+	}()
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Println("elapsed:", elapsed)
 }
--- internal/collector/website_collector.go
+++ internal/collector/website_collector.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -34,7 +35,9 @@ type ChannelResponse struct {
 }
 
 type Collector struct {
-	collector *colly.Collector
+	collector   *colly.Collector
+	accessMutex *sync.Mutex
+	scrollMutex *sync.Mutex
 }
 
 const (
@@ -45,10 +48,14 @@ const (
 )
 
 func NewWebsiteCollector() *Collector {
-	return &Collector{collector: colly.NewCollector(
-		colly.UserAgent(userAgent),
-		colly.AllowedDomains("www.youtube.com", "youtube.com"),
-	)}
+	return &Collector{
+		collector: colly.NewCollector(
+			colly.UserAgent(userAgent),
+			colly.AllowedDomains("www.youtube.com", "youtube.com"),
+		),
+		accessMutex: &sync.Mutex{},
+		scrollMutex: &sync.Mutex{},
+	}
 }
 
 func (c *Collector) CollectWithScrolling(keyword string, numScrolls int) ([]string, error) {
@@ -60,7 +67,7 @@ func (c *Collector) CollectWithScrolling(keyword string, numScrolls int) ([]stri
 		return nil, errors.Wrap(accessErr, fmt.Sprintf("failed to access website by keyword as %s", keyword))
 	}
 
-	scrollDownErr := c.scrollDown(numScrolls, ctx)
+	scrollDownErr := c.scrollDownNTimes(numScrolls, ctx)
 	if scrollDownErr != nil {
 		return nil, errors.Wrap(scrollDownErr, "failed to scroll down")
 	}
@@ -76,6 +83,12 @@ func (c *Collector) CollectWithScrolling(keyword string, numScrolls int) ([]stri
 }
 
 func (c *Collector) accessWebsite(ctx context.Context, keyword string) error {
+	c.accessMutex.Lock()
+	defer c.accessMutex.Unlock()
+
+	fmt.Printf("start to access website by keyword: %s\n", keyword)
+	defer fmt.Printf("end to access website by keyword: %s\n", keyword)
+
 	uri := fmt.Sprintf("%s?search_query=%s&sp=%s", baseUrl, url.QueryEscape(keyword), "EgIQAg%253D%253D")
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(uri),
@@ -107,17 +120,28 @@ func (c *Collector) extractSubscriberNames(ctx context.Context) ([]string, error
 	return subscriberNames, nil
 }
 
-func (c *Collector) scrollDown(numScrolls int, ctx context.Context) error {
+func (c *Collector) scrollDownNTimes(numScrolls int, ctx context.Context) error {
 	for i := 0; i < numScrolls; i++ {
-		runErr := chromedp.Run(ctx,
-			chromedp.Evaluate(scrollDownScript, nil),
-		)
-		if runErr != nil {
-			return errors.Wrap(runErr, "failed to scroll page")
+		err := c.scrollDown(ctx)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("failed to scroll down by n %d times", numScrolls))
 		}
-
-		time.Sleep(1 * time.Second)
 	}
+	return nil
+}
+
+func (c *Collector) scrollDown(ctx context.Context) error {
+	c.scrollMutex.Lock()
+	defer c.scrollMutex.Unlock()
+
+	runErr := chromedp.Run(ctx,
+		chromedp.Evaluate(scrollDownScript, nil),
+	)
+	if runErr != nil {
+		return errors.Wrap(runErr, "failed to scroll page")
+	}
+
+	time.Sleep(1 * time.Second)
 	return nil
 }
 
