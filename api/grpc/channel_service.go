e9dc8385a67851d42e3c17af8627f6c731a629ce
Sujin1135 <mchead1@naver.com>
2025-02-23 20:09:11 +0900

    feat: implemented the rpc logic to get subscriber names by keyword and page size

--- api/grpc/channel_service.go
+++ api/grpc/channel_service.go
@@ -2,20 +2,42 @@ package grpc
 
 import (
 	pb "channel-page-collector/api/grpc/interface/channel-page-collector-interface/protobuf"
+	"channel-page-collector/internal/collector"
 	"github.com/pkg/errors"
 	"log"
+	"sync"
 )
 
 type ChannelService struct {
 	pb.UnimplementedChannelPageServer
+	collector *collector.Collector
 }
 
-func (*ChannelService) GetSubscriberNames(in *pb.GetSubscriberNamesRequest, stream pb.ChannelPage_GetSubscriberNamesServer) error {
-	log.Printf("Received: %v %v\n", in.Keyword, in.Page)
+func NewChannelService() *ChannelService {
+	return &ChannelService{
+		collector: collector.NewWebsiteCollector(),
+	}
+}
 
-	err := stream.Send(&pb.GetSubscriberNamesResponse{Names: []string{in.Keyword}})
-	if err != nil {
-		return errors.Wrap(err, "occurred an error when sending a stream message\n")
+func (c *ChannelService) GetSubscriberNames(in *pb.GetSubscriberNamesRequest, stream pb.ChannelPage_GetSubscriberNamesServer) error {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	subscriberNamesChan := make(chan []string, 1)
+
+	go func() {
+		defer wg.Done()
+		err := c.collector.CollectWithScrolling(in.Keyword, int(in.Page), subscriberNamesChan)
+		if err != nil {
+			log.Fatal("failed to collect subscriber names", err)
+		}
+	}()
+
+	for subscriberNames := range subscriberNamesChan {
+		err := stream.Send(&pb.GetSubscriberNamesResponse{Names: subscriberNames})
+		if err != nil {
+			return errors.Wrap(err, "failed to send subscriber names to grpc client")
+		}
 	}
 
 	return nil
--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,11 @@ package main
 import (
 	grpc2 "channel-page-collector/api/grpc"
 	pb "channel-page-collector/api/grpc/interface/channel-page-collector-interface/protobuf"
-	"channel-page-collector/internal/collector"
 	"flag"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"sync"
-	"time"
 )
 
 var (
@@ -23,37 +20,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	channelService := grpc2.NewChannelService()
 	s := grpc.NewServer()
-	pb.RegisterChannelPageServer(s, &grpc2.ChannelService{})
+	pb.RegisterChannelPageServer(s, channelService)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
-	websiteCollector := collector.NewWebsiteCollector()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	start := time.Now()
-	subscriberNamesChan := make(chan []string, 1)
-
-	go func() {
-		defer wg.Done()
-		fmt.Println("start to collect method 1")
-		err := websiteCollector.CollectWithScrolling("운동", 5, subscriberNamesChan)
-		if err != nil {
-			log.Fatal("failed to collect subscriber names", err)
-		}
-	}()
-
-	fmt.Println("channel data as follow:")
-	for v := range subscriberNamesChan {
-		fmt.Println(v)
-	}
-
-	wg.Wait()
-
-	elapsed := time.Since(start)
-	fmt.Println("elapsed:", elapsed)
 }
